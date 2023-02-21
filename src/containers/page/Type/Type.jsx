1b7190d5329ac84b72f0eea91f0196a62ea9b7ba
unknown <tkiet1705@gmail.com>
2023-02-21 22:27:11 +0700

    fix depen

--- src/containers/page/Type/Type.jsx
+++ src/containers/page/Type/Type.jsx
@@ -1,73 +1,61 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import SkeType from '../../../skeleton/SkeType';
-import './type.css'
-import { Link } from 'react-router-dom';
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import SkeType from "../../../skeleton/SkeType";
+import "./type.css";
+import { Link } from "react-router-dom";
 
 const Render = (props) => {
-    const { item } = props
+    const { item } = props;
     return (
         <div className="col-3 col-md-4 col-sm-6 blog-mg mg-top">
             <div className="video-item type-item">
                 <img src={item.thumbnail} alt="" />
                 <div className="video-play">
-                    <i className='bx bx-play-circle'></i>
+                    <i className="bx bx-play-circle"></i>
                     <span className="time"></span>
                 </div>
-                <Link to={`/detail/${item.categoryId}/${item.name}/${item.id}`} className="video-content">{item.name}</Link>
+                <Link to={`/detail/${item.categoryId}/${item.name}/${item.id}`} className="video-content">
+                    {item.name}
+                </Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
 function Type({ match, props }) {
     console.log(match);
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    const path = match.params.category
-    const type = match.params.type
-
+    const path = match.params.category;
+    const type = match.params.type;
 
     useEffect(() => {
-        setLoading(true)
+        setLoading(true);
         setTimeout(async () => {
             const url = `https://tender-overshirt-colt.cyclic.app/categories/${path}/animes?type=${type}`;
-            const res = await axios(url)
-            setData(res.data)
-            setLoading(false)
+            const res = await axios(url);
+            setData(res.data);
+            setLoading(false);
             console.log(url);
         }, 1500);
-    }, [path])
-
-
+    }, [path, type]);
 
     return (
         <section>
             <div className="container">
                 <div className="path">
-                    <i className='bx bxs-home'></i>
-                    <i className='bx bx-chevron-right'></i>
+                    <i className="bx bxs-home"></i>
+                    <i className="bx bx-chevron-right"></i>
                     <span>{match.params.category}</span>
-                    <i className='bx bx-chevron-right'></i>
+                    <i className="bx bx-chevron-right"></i>
                     <span>{match.params.type}</span>
                 </div>
                 <div className="title-movie">
                     <h3 className="title">Nội dung</h3>
                 </div>
                 <div className="video">
-                    <div className="row wow fadeIn">
-                        {
-                            loading ?
-                                [1, 2, 3, 4, 5, 6, 7, 8].map(item => (
-                                    <SkeType key={item} />
-                                )) :
-                                data.map(item => (
-                                    <Render item={item} key={item.id} />
-                                ))
-                        }
-                    </div>
+                    <div className="row wow fadeIn">{loading ? [1, 2, 3, 4, 5, 6, 7, 8].map((item) => <SkeType key={item} />) : data.map((item) => <Render item={item} key={item.id} />)}</div>
                 </div>
             </div>
         </section>
